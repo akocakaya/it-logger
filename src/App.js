247eeff6d7be77cfa400d7390c795ec764a55be5
Abdullah Kocakaya <akocakaya@protel.com.tr>
2020-03-24 10:30:15 +0300

    redux store and provider setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect } from 'react';
 import M                              from 'materialize-css/dist/js/materialize.min';
+import { Provider } from 'react-redux';
 import 'materialize-css/dist/css/materialize.min.css';
-import './App.css';
 
+import './App.css';
 import SearchBar     from './components/layout/SearchBar';
 import Logs          from './components/logs/Logs';
 import AddBtn        from './components/layout/AddBtn';
@@ -10,6 +11,8 @@ import AddLogModal   from './components/logs/AddLogModal';
 import EditLogModal  from './components/logs/EditLogModal';
 import AddTechModal  from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+
+import store from './store';
 
 const App = () => {
 
@@ -19,7 +22,7 @@ const App = () => {
     } );
 
     return (
-        <Fragment>
+        <Provider store={store}>
             <SearchBar />
             <div className = { 'container' }>
                 <AddBtn/>
@@ -29,7 +32,7 @@ const App = () => {
                 <TechListModal/>
                 <Logs />
             </div>
-        </Fragment>
+        </Provider>
     );
 };
 
