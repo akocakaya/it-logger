b8a777f15aa99c98010c55b94b8631992d5519b7
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2021-01-04 23:45:25 +0300

    readme added

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -4,7 +4,7 @@ import {connect}           from 'react-redux';
 import {addTechs}          from '../../actions/techActions';
 
 const AddTechModal = ({addTechs}) => {
-    const [ firstName, setFirstName ]     = useState( '' );
+    const [ firstName, setFirstName ] = useState( '' );
     const [ lastName, setLastName ] = useState( '' );
 
     const onSubmit = () => {
